8cddb5a508d60ea9c8358d7ed0d1eaa77947f56b
Ebendttl <ebendttl@gmail.com>
2024-08-12 17:39:15 +0100

    Task 0: Fix 1

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""
-A function to return a list of integers
-Returns an empty list if n <= 0
-Can assume n will be always an integer
-"""
+"""A script to determine pascal's triangle for any number"""
+
 
 def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing the Pascal's triangle of n
+    """
+    triangle = []
+
     if n <= 0:
-        return []
-    
-    triangle = [[1]]
-    
-    for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]
-        
-        for j in range(1, i):
-            new_row.append(prev_row[j-1] + prev_row[j])
-        
-        new_row.append(1)
-        triangle.append(new_row)
-    
+        return triangle
+    for x in range(n):
+        row = []
+
+        for y in range(x+1):
+            if y == 0 or y == x:
+                row.append(1)
+            else:
+                row.append(triangle[x-1][y-1] + triangle[x-1][y])
+        triangle.append(row)
+
     return triangle
 
